172b78dc6ab3d7b0537abaca8553665fd0bd30e1
Adam Forester <aforester@silverdogstudios.com>
2019-05-01 13:06:13 -0500

    session based update

--- remove_tag.sh
+++ remove_tag.sh
@@ -14,9 +14,12 @@ printf "There are $total objects\n"
 printf "\nGenerting Script to remove $TAG tag\n"
 for I in $(seq 0 500 $total)
 do
-mgmt_cli -r true show objects offset $I limit 500 details-level full filter "$TAG" --format json | jq --raw-output --arg Q "'" --arg TAG "$TAG" '.objects[] | select(.tags[].name==$TAG) | "mgmt_cli -r true set " + .type + " name " + .name + " tags.remove " + $Q + .tags[].name + $Q' >>tag_remove_$TAG.txt
+mgmt_cli -r true show objects offset $I limit 500 details-level full filter "$TAG" --format json | jq --raw-output --arg Q "'" --arg TAG "$TAG" '.objects[] | select(.tags[].name==$TAG) | "mgmt_cli -s id.txt set " + .type + " name " + $Q + .name + $Q + " tags.remove " + $Q + .tags[].name + $Q' >>tag_remove_$TAG.txt
 done
 
 chmod +x tag_remove_$TAG.txt
+sed -i '1s/^/mgmt_cli -r true login > id.txt\n/' tag_remove_$TAG.txt
+echo "mgmt_cli -s id.txt publish" >> tag_remove_$TAG.txt
+echo "mgmt_cli -s id.txt logout" >> tag_remove_$TAG.txt
 
 printf "\nYou can execute file tag_remove_$TAG.txt to remove tags\n"
